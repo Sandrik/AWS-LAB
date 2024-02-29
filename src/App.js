290f68de9c1b935602195d4165182b27133aaf15
Sandrik <your>
2024-02-29 10:01:31 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './NavBar.js';
+import './Card.js';
 
 function App() {
   return (
@@ -8,11 +10,16 @@ function App() {
         <h1>CS 230L</h1>
         <h2>Section - 003 </h2>
         <p>WVU ID: 800256647</p>
-        <p>Hi I am Sandrik Tabidze</p>
+        <div className="card-container">
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
 
