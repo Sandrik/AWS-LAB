dfe397695fb7a3eb967a469d6f20e7b8b20b4f00
Sandrik <your>
2024-02-29 09:16:44 -0500

    Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>CS 230L</h1>
+        <h2>Section - 003 </h2>
+        <p>WVU ID: 800256647</p>
+        <p>Hi I am Sandrik Tabidze</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
