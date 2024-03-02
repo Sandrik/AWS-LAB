8fa0e78b06d69873851942785c72e9e06b17db66
Sandrik <your>
2024-03-02 13:42:25 -0500

    commit

--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import './Card.css';
 
-
-function Card(){
-    return(
-<>
-<div class="card text-bg-primary mb-3" style={{maxWidth: '18rem'}}>
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Primary card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-bg-secondary mb-3" style={{maxWidth: '18rem'}}>
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Secondary card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-bg-success mb-3" style={{maxWidth: '18rem'}}>
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Success card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-</>
-    );
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="card text-bg-primary mb-3">
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <div className="card text-bg-secondary mb-3">
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Secondary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <div className="card text-bg-success mb-3">
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Success card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
