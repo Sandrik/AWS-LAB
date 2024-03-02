07fdf486d74cae9e8799eab49e7c5b7f7885b249
Sandrik <your>
2024-03-02 14:07:16 -0500

    commit

--- src/NavBar.js
+++ src/NavBar.js
@@ -19,11 +19,8 @@ function NavBar() {
             <li class="nav-item">
               <a class="nav-link" href="#">Contact</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
           </ul>
-          {/* Search form with ms-auto class */}
+          {}
           <form class="d-flex ms-auto">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button class="btn btn-outline-success" type="submit">Search</button>
