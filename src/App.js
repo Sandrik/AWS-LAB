8e7d436e9299d95912d2846613180d27c42447ef
Sandrik <your>
2024-03-02 13:52:18 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Card from './Card.js';
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header>
         <h1>CS 230L</h1>
         <h2>Section - 003 </h2>
         <p>WVU ID: 800256647</p>
         <p>Hi I am Sandrik Tabidze!</p>
       </header>
-      <NavBar />
       <div>
         <Card />
       </div>
