74151ad87933a920c4620b4f7a63bdd164751feb
Sandrik <your>
2024-03-02 13:17:48 -0500

    commit

--- src/Card.js
+++ src/Card.js
@@ -3,21 +3,21 @@ import React from 'react';
 function Card(){
     return(
 <>
-<div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+<div class="card text-bg-primary mb-3" style={{maxWidth: '18rem'}}>
   <div class="card-header">Header</div>
   <div class="card-body">
     <h5 class="card-title">Primary card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
-<div class="card text-bg-secondary mb-3" style="max-width: 18rem;">
+<div class="card text-bg-secondary mb-3" style={{maxWidth: '18rem'}}>
   <div class="card-header">Header</div>
   <div class="card-body">
     <h5 class="card-title">Secondary card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
-<div class="card text-bg-success mb-3" style="max-width: 18rem;">
+<div class="card text-bg-success mb-3" style={{maxWidth: '18rem'}}>
   <div class="card-header">Header</div>
   <div class="card-body">
     <h5 class="card-title">Success card title</h5>
@@ -29,3 +29,4 @@ function Card(){
 }
 
 export default Card;
+
