997ff9badea30866be30d3fdf469297273d025bb
Sandrik <your>
2024-03-02 14:00:36 -0500

    commit

--- src/Card.js
+++ src/Card.js
@@ -5,21 +5,21 @@ function Card() {
   return (
     <div className="card-container">
       <div className="card text-bg-primary mb-3">
-        <div className="card-header">Header</div>
+        <div className="card-header">Card 1</div>
         <div className="card-body">
           <h5 className="card-title">Primary card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
       <div className="card text-bg-secondary mb-3">
-        <div className="card-header">Header</div>
+        <div className="card-header">Card 2</div>
         <div className="card-body">
           <h5 className="card-title">Secondary card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
       <div className="card text-bg-success mb-3">
-        <div className="card-header">Header</div>
+        <div className="card-header">Card 3</div>
         <div className="card-body">
           <h5 className="card-title">Success card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
